679716cd2c83334f48ae18f32508bf1db9d6c7ff
Clément Bœsch <u@pkh.me>
2026-09-05 20:04:52 +0200

    Add heuristics to improve OCR

--- snapstudysensei/ocr.py
+++ snapstudysensei/ocr.py
@@ -1,5 +1,5 @@
 from manga_ocr import MangaOcr
-from PIL import Image
+from PIL import Image, ImageOps, ImageStat
 
 
 class OCRWrapper:
@@ -7,4 +7,9 @@ class OCRWrapper:
         self._mocr = MangaOcr()
 
     def __call__(self, image: Image.Image) -> str:
+        image = ImageOps.autocontrast(ImageOps.grayscale(image))
+        # Assume the background covers most of the crop, and prefer dark text
+        # on a light background.
+        if ImageStat.Stat(image).median[0] < 128:
+            image = ImageOps.invert(image)
         return self._mocr(image)
